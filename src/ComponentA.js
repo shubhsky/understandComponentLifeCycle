d9c2a0301ae5ae38b1161807b396a085e591167e
Shubham Kumar <shubhamkr15060@gmail.com>
2023-10-04 11:58:43 +0530

    using getch API

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -5,7 +5,8 @@ class ComponentA extends React.Component{
     constructor(){
         super();
         this.state={
-            name:'ComponentA'
+            name:'ComponentA',
+            data:[]
         }
         console.log('Component A constructor')
         // this.setState({
@@ -21,18 +22,31 @@ class ComponentA extends React.Component{
         return null;
     }
 
+    // https://jsonplaceholder.typicode.com/users
+
     componentDidMount(){
         console.log('Component A component did mount')
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+                .then(data => this.setState({data}))
     }
 
     render(){
         console.log('Component A render')
+        // console.log(this.state.data)
         // this.setState({
         //     name:'CA'
         // })
         return(
             <>
-                <h1>{this.state.name}</h1>  
+                <h1>{this.state.name}</h1>
+                <ul>
+                    {this.state.data.map((d)=>{
+                        return(
+                            <li>{d.name}</li>
+                        )
+                    })}
+                </ul>
                 <ComponentB/>
             </>
         )
